36dc86c2139dc106bbcc6261835c31fce68c84d1
yamaai2yamaai <shapeyamaaiservices@gmail.com>
2023-03-16 16:15:32 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - prints without the use of puts command
+ *
+ * Return: 0 after execution
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
