b7c8cdde84204fd6cd76868ff20494f3b95ee1f9
yamaai2yamaai <shapeyamaaiservices@gmail.com>
2023-03-20 16:31:10 -0700

    persistance patience and perspirations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (successfully)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
