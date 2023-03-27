adbb095bab7a10763587d6b57767adab1fba9e9f
yamaai2yamaai <shapeyamaaiservices@gmail.com>
2023-03-27 16:31:14 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - function to count string
+ * @s: string to be returned
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
