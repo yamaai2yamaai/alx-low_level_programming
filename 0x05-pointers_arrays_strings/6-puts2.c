a7c0898897f6d4e9160f4006b0115cf0f7f91d20
yamaai2yamaai <shapeyamaaiservices@gmail.com>
2023-03-27 17:31:12 -0700

    puts printing

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints string function
+ * @str: function parameter
+ *
+ * Return: 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
