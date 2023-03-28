b83787cd0ebdf0f588e4aa83f1c96678e3a2b900
yamaai2yamaai <shapeyamaaiservices@gmail.com>
2023-03-27 17:10:38 -0700

    print in reverse puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints strings in reverse
+ * @s: function parameter
+ *
+ * Return: 0.
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
